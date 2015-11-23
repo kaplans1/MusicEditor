7f33a5734bae80c7fcc557f381e502b686d05dab
Nat Dempkowski <natdempk@gmail.com>
2015-11-23 17:05:28 -0500

    Fix malformed comment

--- src/cs3500/music/controller/MusicController.java
+++ src/cs3500/music/controller/MusicController.java
@@ -76,9 +76,14 @@ public class MusicController {
   }
 
   /**
-   * TODO: delete a note c5 starting on beat 64 c5 d 64 add a note in c5 on beat 64 lasting 5 beats
-   * c5 a 64 5 move a note in c5 on beat c4 to d3 on beat 75 maintaining length c5 m 64 d3 75 arrow
-   * keys to scroll through composition based on offset space bar to play/pause/restart composition
+   * TODO:
+   * delete a note c5 starting on beat 64
+   * c5 x 64
+   * add a note in c5 on beat 64 lasting 5 beats
+   * c5 n 64 5
+   * move a note in c5 on beat c4 to d3 on beat 75 maintaining length
+   * c5 m 64 d3 75
+   * arrow keys to scroll through composition based on offset space bar to play/pause/restart composition
    * home/end keys to move to the beginning/end of composition
    */
 
